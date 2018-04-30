c1331007dc3cf2eca1cae719b640499db7eff987
akshayk <akshay@cs.umass.edu>
2018-04-30 10:05:54 -0400

    bag greedy

--- LangevinCB.py
+++ LangevinCB.py
@@ -6,7 +6,6 @@ from Util import *
 
 class LangevinCB(Semibandits.Semibandit):
 
-
     def __init__(self, B, model=NNModels.LinearModel):
         self.B = B
         self.model = model(B.d, B.K)
@@ -49,7 +48,6 @@ class LangevinCB(Semibandits.Semibandit):
         feat = x.get_ld_features()
         self.X[self.t,0,:] = feat.reshape(1,self.d*self.K)
         
-
     def _sample(self, x, p_vec):
         unif = np.random.binomial(1, self._get_mu())
         if unif:
@@ -64,7 +62,7 @@ class LangevinCB(Semibandits.Semibandit):
         NNModels.langevin_step(self.model, self.X[0:self.t,:,:], self.R[0:self.t,:,:], self.lr, iters=self.burn_in, noise=True)
         p_vec = NNModels.model_to_action(self.model, x)
         self.action = self._sample(x, p_vec)
-        ## Compute importanc weight for this action
+        ## Compute importance weight for this action
         M = 1
         while True:
             NNModels.langevin_step(self.model, self.X[0:self.t,:,:], self.R[0:self.t,:,:], self.lr, iters=self.step, noise=True)
@@ -86,8 +84,85 @@ class LangevinCB(Semibandits.Semibandit):
         return (np.min([1,c]))
 
 
+class BagCB(Semibandits.Semibandit):
+
+    def __init__(self, B, model=NNModels.LinearModel):
+        self.B = B
+        self.model = model
+        
+    def init(self, T, params={}):
+        if 'schedule' in params.keys():
+            self.training_points = training_points(T, schedule=params['schedule'])
+        else:
+            self.training_points = training_points(T)
+
+        self.reward = []
+        self.opt_reward = []
+        self.d = self.B.d
+        self.K = self.B.K
+
+        self.T = T
+        self.action = None
+        self.imp_weights = None
+        self.t = 1
+        
+        ## Exploration probability
+        if 'mu' in params.keys():
+            self.mu = float(params['mu'])
+        else:
+            self.mu = 0.00
+
+        ## Learning rate
+        if 'lr' in params.keys():
+            self.lr = float(params['lr'])
+        else:
+            self.lr = 0.1
+
+        if 'M' in params.keys():
+            self.M = int(params['M'])
+        else:
+            self.M = 20
+        self.models = [self.model(self.B.d, self.B.K) for i in range(self.M)]
+
+
+    def update(self, x, A, y_vec, r):
+        full_rvec = np.zeros(x.get_K())
+        full_rvec[A] = y_vec/self.imp_weights[A]
+        feat = x.get_ld_features()
+        X = np.zeros((1,1,self.d*self.K))
+        X[0,0,:] = feat.reshape(1,self.d*self.K)
+        full_rvec = full_rvec.reshape(1,1,self.K)
+        for model in self.models:
+            NNModels.langevin_step(model, X, full_rvec, self.lr, iters=int(np.random.poisson(lam=1.0,size=1)), noise=True)
+        
+    def _sample(self, x, p_vec):
+        unif = np.random.binomial(1, self._get_mu())
+        if unif:
+            act = np.random.choice(x.get_K(), size=x.get_L(), replace=False)
+        else:
+            draw = np.random.multinomial(1, p_vec)
+            act = np.where(draw)[0][0]
+        return act
+
+    def get_action(self,x):
+        ## Run Langevin Gradient
+        p_vecs = [NNModels.model_to_action(self.models[i], x) for i in range(self.M)]
+        p_vec = np.mean(p_vecs,axis=0)
+        self.action = self._sample(x, p_vec)
+
+        self.imp_weights = np.zeros(x.get_K())
+        self.imp_weights[self.action] = (1-self._get_mu())*p_vec[self.action] + self._get_mu()/x.get_K()
+        return [self.action]
+
+    def _get_mu(self):
+        a = self.mu
+        b = self.mu*np.sqrt(self.B.K)/np.sqrt(self.t)
+        c = np.min([a,b])
+        return (np.min([1,c]))
+
 if __name__=='__main__':
-    S = Simulators.LinearBandit(10, 1, 5, noise=True)
+    S = Simulators.LinearBandit(10, 1, 2, noise=True, pos=False)
     
-    L = LangevinCB(S)
-    L.play(1000)
+    # L = LangevinCB(S)
+    L = BagCB(S)
+    L.play(5000, params={'M': 10})
